f96d72ee82f1320a26801d28459989782abe56f9
David Castro <castrolimoeiro@gmail.com>
2022-03-27 10:50:42 -0300

    alteration_1.2

--- 25-sistema_login/index.php
+++ 25-sistema_login/index.php
@@ -1,7 +1,45 @@
 <?php
+// conexão
+require_once 'db_connect.php';
+
+//session
+session_start();
+
 // Botão enviar
 if(isset($_POST['btn-entrar'])):
-    echo "clicou";
+    $erros = array();
+    $login = mysqli_escape_string($connect, $_POST['login']);
+    $senha = mysqli_escape_string($connect, $_POST['senha']);
+
+    if(empty($login) or empty($senha)):
+        $erros[] = "<li> Preencha os campos para realizar o login. </li>";
+    else:
+        $sql = "SELECT login FROM usuarios WHERE login  = '$login'";
+        $resultado = mysqli_query($connect, $sql);
+
+        if(mysqli_num_rows($resultado) > 0):
+            $senha = md5($senha);
+            $sql = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha' ";
+            $resultado = mysqli_query($connect, $sql);
+                
+            if(mysqli_num_rows($resultado) == 1):
+                    $dados = mysqli_fetch_array($resultado);
+                    mysqli_close($connect);
+                    $_SESSION['logado'] = true;
+                    $_SESSION['id_usuario'] = $dados['id'];
+                    header('Location: home.php');
+                else:
+                    $erros[] = "<li> Usuário ou senha não conferem. </li>";
+                endif;
+
+        else:
+            $erros[] = "<li> Usuário inexistente! </li>";
+        endif;
+
+    
+    endif;
+
+
 endif;
 ?>
 
@@ -10,10 +48,20 @@ endif;
 <html lang="en">
 <body>
     <h1>Login</h1>
+
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
         Login: <input type="text" name="login"><br>
-        Senha: <input type="password" name="senha"><br>
+        Senha: <input type="password" name="senha"><br><br>
         <button type="submit" name="btn-entrar"> Entrar </button>
+    
+    <?php 
+    echo "<hr>"; 
+    if (!empty($erros)):
+        foreach($erros as $erro):
+            echo $erro;
+        endforeach;
+    endif;
+    ?>
     </form>
 </body>
 </html>
